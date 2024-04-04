69af068a2acfcae06cf3c19acc2122e8e94973fc
Ilya Mateyko <me@astrophena.name>
2024-04-05 00:10:25 +0300

    cmd/nora/play: add starting bits of Nora Playground

---
+++ cmd/nora/play/main.go
@@ -0,0 +1,43 @@
+// play implements a Nora playground, running on WebAssembly in browser.
+package main
+
+import (
+	"fmt"
+	"strings"
+	"syscall/js"
+
+	"go.astrophena.name/tools/internal/nora/lex"
+	"go.astrophena.name/tools/internal/nora/token"
+)
+
+func runNora() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			return "no arguments passed"
+		}
+
+		doc := js.Global().Get("document")
+		if !doc.Truthy() {
+			return "unable to get document object"
+		}
+		outputArea := doc.Call("getElementById", "output")
+		if !outputArea.Truthy() {
+			return "unable to get output text area"
+		}
+
+		input := args[0].String()
+		l := lex.New(input)
+
+		var sb strings.Builder
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(&sb, "%+v\n", tok)
+		}
+		outputArea.Set("value", sb.String())
+		return nil
+	})
+}
+
+func main() {
+	js.Global().Set("runNora", runNora())
+	<-make(chan struct{})
+}
