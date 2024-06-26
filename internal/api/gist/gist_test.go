42f53ecd9f4c87b82946c51ec958601eecec6e2a
Ilya Mateyko <me@astrophena.name>
2024-06-26 21:23:09 +0300

    internal/api/gist: add record-replay test for c.Update

--- internal/api/gist/gist_test.go
+++ internal/api/gist/gist_test.go
@@ -3,6 +3,7 @@ package gist
 import (
 	"context"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -11,8 +12,8 @@ import (
 	"go.astrophena.name/tools/internal/testutil"
 )
 
-func TestGetSiteGist(t *testing.T) {
-	rec, err := rr.Open(filepath.Join("testdata", "site.httprr"), http.DefaultTransport)
+func TestGet(t *testing.T) {
+	rec, err := rr.Open(filepath.Join("testdata", "get.httprr"), http.DefaultTransport)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -34,6 +35,53 @@ func TestGetSiteGist(t *testing.T) {
 	name, file := getFirstFile(gist)
 	testutil.AssertEqual(t, name, "site.go")
 	testutil.AssertEqual(t, strings.HasPrefix(file.Content, "package main"), true)
+}
+
+// Updating this test:
+//
+//	$ GITHUB_TOKEN="$(gh auth token)" go test -httprecord testdata/update.httprr
+//
+
+func TestUpdate(t *testing.T) {
+	rec, err := rr.Open(filepath.Join("testdata", "update.httprr"), http.DefaultTransport)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rec.Scrub(func(r *http.Request) error {
+		r.Header.Del("Authorization")
+		return nil
+	})
+
+	c := &Client{
+		HTTPClient: rec.Client(),
+	}
+
+	// See https://gist.github.com/astrophena/a91d766ec189326040f0a491243a86b1 for
+	// the gist we are testing against.
+	const id = "a91d766ec189326040f0a491243a86b1"
+
+	if rec.Recording() {
+		c.Token = os.Getenv("GITHUB_TOKEN")
+	}
+
+	gist, err := c.Get(context.Background(), id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Initial gist contents.
+	testutil.AssertEqual(t, len(gist.Files), 1)
+	name, file := getFirstFile(gist)
+	testutil.AssertEqual(t, name, "foo.txt")
+	testutil.AssertEqual(t, file.Content, "bar\n")
+
+	// Add a file and update our gist.
+	gist.Files["bar.txt"] = File{Content: "foo\n"}
+	gist, err = c.Update(context.Background(), id, gist)
+	if err != nil {
+		t.Fatal(err)
+	}
+	testutil.AssertEqual(t, len(gist.Files), 2)
 }
 
 func getFirstFile(g *Gist) (name string, file File) {
