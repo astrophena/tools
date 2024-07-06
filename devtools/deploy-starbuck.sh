51312e9fb8b02380f2299bbdca49482d43d92afc
Ilya Mateyko <me@astrophena.name>
2024-07-06 22:01:08 +0300

    cmd/starbuck: fix systemd service

--- devtools/deploy-starbuck.sh
+++ devtools/deploy-starbuck.sh
@@ -6,10 +6,23 @@ cleanup() {
 }
 trap cleanup INT EXIT
 
+run() {
+	ssh astrophena@exp.astrophena.name "$@"
+}
+
+# Cross-compile Starbuck binary. Why? Because most of the time this script runs
+# on my ARM64 Android tablet.
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w -buildid=" -trimpath -o "$tmpdir/starbuck" ./cmd/starbuck
+
+# Copy built binary and systemd service to the server.
 scp "$tmpdir/starbuck" "astrophena@exp.astrophena.name:"
 scp cmd/starbuck/starbuck.service "astrophena@exp.astrophena.name:"
-ssh astrophena@exp.astrophena.name doas install -m755 -o root -g root starbuck /usr/local/bin/starbuck
-ssh astrophena@exp.astrophena.name doas install -m644 -o root -g root starbuck.service /etc/systemd/system/starbuck.service
-ssh astrophena@exp.astrophena.name doas systemctl daemon-reload
-ssh astrophena@exp.astrophena.name doas systemctl restart starbuck
+run doas install -m755 -o root -g root starbuck /usr/local/bin/starbuck
+run doas install -m644 -o root -g root starbuck.service /etc/systemd/system/starbuck.service
+
+# Reload systemd state and restart Starbuck.
+run doas systemctl daemon-reload
+run doas systemctl restart starbuck
+
+# Remove leftovers from deploy.
+run rm /home/astrophena/starbuck /home/astrophena/starbuck.service
