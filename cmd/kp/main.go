5f94e72c317ebbdc7a2671540b33caa0eb5a805f
Ilya Mateyko <me@astrophena.name>
2024-10-13 10:42:02 +0300

    cmd/kp: support format template and new -l flag

--- cmd/kp/main.go
+++ cmd/kp/main.go
@@ -6,9 +6,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"flag"
 	"fmt"
+	"html/template"
 	"io"
 	"os"
 	"syscall"
@@ -29,8 +32,13 @@ func run(args []string, stdin io.Reader, stdout, stderr io.Writer) error {
 	a := &cli.App{
 		Name:        "kp",
 		Description: helpDoc,
-		ArgsUsage:   "[flags...] <file> <entry>",
+		ArgsUsage:   "[flags...] <file> [entry]",
+		Flags:       flag.NewFlagSet("kp", flag.ContinueOnError),
 	}
+	var (
+		format = a.Flags.String("f", "{{ .GetPassword }}", "format template")
+		list   = a.Flags.Bool("l", false, "list all entries")
+	)
 	if err := a.HandleStartup(args, stdout, stderr); err != nil {
 		if errors.Is(err, cli.ErrExitVersion) {
 			return nil
@@ -38,13 +46,21 @@ func run(args []string, stdin io.Reader, stdout, stderr io.Writer) error {
 		return err
 	}
 
+	tmpl, err := template.New("main").Parse(*format)
+	if err != nil {
+		return fmt.Errorf("invalid format: %v", err)
+	}
+
 	fargs := a.Flags.Args()
-	if len(fargs) != 2 {
+	if len(fargs) == 0 {
+		a.Flags.Usage()
+		return fmt.Errorf("%w: missing required argument 'file'", cli.ErrInvalidArgs)
+	} else if !*list && len(fargs) < 2 {
 		a.Flags.Usage()
 		return fmt.Errorf("%w: missing required arguments 'file' and/or 'entry'", cli.ErrInvalidArgs)
 	}
 
-	file, entry := fargs[0], fargs[1]
+	file := fargs[0]
 
 	f, err := os.Open(file)
 	if err != nil {
@@ -57,24 +73,59 @@ func run(args []string, stdin io.Reader, stdout, stderr io.Writer) error {
 		return err
 	}
 
-	e, err := lookup(f, password, entry)
+	if *list {
+		db, err := open(f, password)
+		if err != nil {
+			return err
+		}
+		for _, g := range db.Content.Root.Groups {
+			for _, e := range g.Entries {
+				if err := printEntry(tmpl, &e, stdout); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	}
+
+	e, err := lookup(f, password, fargs[1])
 	if err != nil {
 		return err
 	}
-	fmt.Fprintln(stdout, e.GetPassword())
+	if err := printEntry(tmpl, e, stdout); err != nil {
+		return err
+	}
 
+	return nil
+}
+
+func printEntry(tmpl *template.Template, e *gokeepasslib.Entry, stdout io.Writer) error {
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, e); err != nil {
+		return err
+	}
+	buf.WriteString("\n")
+	buf.WriteTo(stdout)
 	return nil
 }
 
 var errNotFound = errors.New("not found")
 
-func lookup(r io.Reader, password string, entry string) (*gokeepasslib.Entry, error) {
+func open(r io.Reader, password string) (*gokeepasslib.Database, error) {
 	db := gokeepasslib.NewDatabase()
 	db.Credentials = gokeepasslib.NewPasswordCredentials(password)
 	if err := gokeepasslib.NewDecoder(r).Decode(db); err != nil {
 		return nil, err
 	}
 	if err := db.UnlockProtectedEntries(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func lookup(r io.Reader, password string, entry string) (*gokeepasslib.Entry, error) {
+	db, err := open(r, password)
+	if err != nil {
 		return nil, err
 	}
 	for _, g := range db.Content.Root.Groups {
