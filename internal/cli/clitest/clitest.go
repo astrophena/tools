e92f885375611d62ac1a023d9c7e6c05a1b35ad2
Ilya Mateyko <me@astrophena.name>
2024-11-25 16:32:28 +0300

    internal/cli/clitest: run 'go generate'

--- internal/cli/clitest/clitest.go
+++ internal/cli/clitest/clitest.go
@@ -1,3 +1,7 @@
+// © 2024 Ilya Mateyko. All rights reserved.
+// Use of this source code is governed by the ISC
+// license that can be found in the LICENSE.md file.
+
 // Package clitest provides utilities for testing command-line applications.
 package clitest
 
