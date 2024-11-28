ae5e99f9dc87064179ef2e867c5b5184064ea80d
Ilya Mateyko <me@astrophena.name>
2024-11-28 21:31:46 +0300

    devtools: add a Git hook for pre-commit check

--- ci_test.go
+++ ci_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the ISC
 // license that can be found in the LICENSE.md file.
 
-package devtools
+package tools
 
 //go:generate go run devtools/gen/copyright.go
 //go:generate go run devtools/gen/readme.go
