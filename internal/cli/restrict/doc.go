83065c76adc562bf33694a10b3370068c75c1e8c
Ilya Mateyko <me@astrophena.name>
2024-12-10 19:52:17 +0300

    internal/cli/restrict: add copyrights

--- internal/cli/restrict/doc.go
+++ internal/cli/restrict/doc.go
@@ -1,3 +1,7 @@
+// © 2024 Ilya Mateyko. All rights reserved.
+// Use of this source code is governed by the ISC
+// license that can be found in the LICENSE.md file.
+
 // Package restrict allows programs to use [Landlock] LSM on supported systems
 // for sandboxing. On unsupported systems it does nothing.
 package restrict
