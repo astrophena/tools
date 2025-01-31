ee682d3b21f2a5e0e2fb0ad52bb1a1b89a2b8913
Ilya Mateyko <me@astrophena.name>
2025-01-31 08:54:46 +0000

    cmd/starlet/internal/geminiproxy: add copyright headers

--- cmd/starlet/internal/geminiproxy/geminiproxy.go
+++ cmd/starlet/internal/geminiproxy/geminiproxy.go
@@ -1,3 +1,7 @@
+// © 2025 Ilya Mateyko. All rights reserved.
+// Use of this source code is governed by the ISC
+// license that can be found in the LICENSE.md file.
+
 // Package geminiproxy implements an HTTP handler to proxy Gemini API requests.
 package geminiproxy
 
