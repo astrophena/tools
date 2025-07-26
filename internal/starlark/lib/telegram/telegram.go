261428c6c1ed23e3b04863792621b07a4eb69c6b
Ilya Mateyko <me@astrophena.name>
2025-07-26 22:07:56 +0300

    cmd/starlet: document image argument for gemini.generate_content

--- internal/starlark/lib/telegram/telegram.go
+++ internal/starlark/lib/telegram/telegram.go
@@ -20,6 +20,11 @@ import (
 	"go.starlark.net/starlarkstruct"
 )
 
+const (
+	tgURL     = "https://api.telegram.org/bot"
+	tgFileURL = "https://api.telegram.org/file/bot"
+)
+
 // Module returns a Starlark module that exposes the Telegram Bot API.
 //
 // This module provides two functions: call and get_file.
@@ -97,7 +102,7 @@ func (m *module) call(thread *starlark.Thread, b *starlark.Builtin, args starlar
 	ctx := interpreter.Context(thread)
 	rawResp, err := request.Make[json.RawMessage](ctx, request.Params{
 		Method: http.MethodPost,
-		URL:    "https://api.telegram.org/bot" + m.token + "/" + string(method),
+		URL:    tgURL + m.token + "/" + string(method),
 		Body:   json.RawMessage(rawReq),
 		Headers: map[string]string{
 			"User-Agent": version.UserAgent(),
@@ -118,31 +123,41 @@ func (m *module) getFile(thread *starlark.Thread, b *starlark.Builtin, args star
 		return nil, err
 	}
 
+	ctx := interpreter.Context(thread)
+
 	type fileInfo struct {
 		Result struct {
 			FilePath string `json:"file_path"`
 		} `json:"result"`
 	}
 
-	fi, err := request.Make[fileInfo](interpreter.Context(thread), request.Params{
+	fi, err := request.Make[fileInfo](ctx, request.Params{
 		Method: http.MethodPost,
-		URL:    "https://api.telegram.org/bot" + m.token + "/getFile",
+		URL:    tgURL + m.token + "/getFile",
+		Headers: map[string]string{
+			"User-Agent": version.UserAgent(),
+		},
 		Body: map[string]string{
 			"file_id": fileID,
 		},
-		Scrubber: m.scrubber,
+		HTTPClient: m.httpc,
+		Scrubber:   m.scrubber,
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: failed to call getFile on file %q: %v", b.Name(), fileID, err)
 	}
 
-	buf, err := request.Make[request.Bytes](interpreter.Context(thread), request.Params{
-		Method:   http.MethodGet,
-		URL:      "https://api.telegram.org/file/bot" + m.token + "/" + fi.Result.FilePath,
-		Scrubber: m.scrubber,
+	buf, err := request.Make[request.Bytes](ctx, request.Params{
+		Method: http.MethodGet,
+		URL:    tgFileURL + m.token + "/" + fi.Result.FilePath,
+		Headers: map[string]string{
+			"User-Agent": version.UserAgent(),
+		},
+		HTTPClient: m.httpc,
+		Scrubber:   m.scrubber,
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: failed to download file %q: %v", b.Name(), fileID, err)
 	}
 
 	return starlark.Bytes(buf), nil
