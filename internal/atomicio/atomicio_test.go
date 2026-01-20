0c09c3b425850239c494c81b3f0c1f0d9af677f8
Ilya Mateyko <me@astrophena.name>
2026-01-20 10:12:24 +0300

    internal/atomicio: actually, it's 2026 in tests

--- internal/atomicio/atomicio_test.go
+++ internal/atomicio/atomicio_test.go
@@ -1,4 +1,4 @@
-// © 2024 Ilya Mateyko. All rights reserved.
+// © 2026 Ilya Mateyko. All rights reserved.
 // Use of this source code is governed by the ISC
 // license that can be found in the LICENSE.md file.
 
