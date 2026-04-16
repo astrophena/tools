393e31270b47c5e90a80c14124c74a4a9f3e4206
Ilya Mateyko <me@astrophena.name>
2026-04-16 20:25:51 +0300

    .devtools/gha-deploy-service.bash: strip binaries (#327)

--- .devtools/gha-deploy-service.bash
+++ .devtools/gha-deploy-service.bash
@@ -19,6 +19,7 @@ mkdir -p "$build_dir/build/bin" "$build_dir/build/etc/systemd/system"
 # See https://github.com/golang/go/issues/44695#issuecomment-973685193.
 CGO_ENABLED=1 GOOS=linux CC="zig cc -target x86_64-linux-musl" GOOS=linux GOARCH=amd64 \
 	go build -ldflags="-linkmode=external -s -w -buildid=" -trimpath -o "$build_dir/build/bin/$service" "./cmd/$service"
+strip "$build_dir/build/bin/$service"
 cp cmd/$service/systemd/* "$build_dir/build/etc/systemd/system"
 tar -czf "$build_dir/archive.tar.gz" -C "$build_dir/build" .
 go tool deploy -type service "$service" "$build_dir/archive.tar.gz"
