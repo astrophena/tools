8026628cbe4363ef0b6254e928652df1c0efb388
Ilya Mateyko <me@astrophena.name>
2026-04-16 20:19:39 +0300

    .devtools/gha-deploy-service.bash: fix build with Zig (#326)

--- .devtools/gha-deploy-service.bash
+++ .devtools/gha-deploy-service.bash
@@ -16,8 +16,9 @@ cleanup() {
 trap cleanup EXIT
 
 mkdir -p "$build_dir/build/bin" "$build_dir/build/etc/systemd/system"
+# See https://github.com/golang/go/issues/44695#issuecomment-973685193.
 CGO_ENABLED=1 GOOS=linux CC="zig cc -target x86_64-linux-musl" GOOS=linux GOARCH=amd64 \
-	go build -ldflags="-s -w -buildid=" -trimpath -o "$build_dir/build/bin/$service" "./cmd/$service"
+	go build -ldflags="-linkmode=external -s -w -buildid=" -trimpath -o "$build_dir/build/bin/$service" "./cmd/$service"
 cp cmd/$service/systemd/* "$build_dir/build/etc/systemd/system"
 tar -czf "$build_dir/archive.tar.gz" -C "$build_dir/build" .
 go tool deploy -type service "$service" "$build_dir/archive.tar.gz"
